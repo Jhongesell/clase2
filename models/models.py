80a90ebd86e673be7863d87e55018681d0fc5f6d
Victor Daniel Moreno Lopez <danielvdmlfiis@gmail.com>
2017-11-19 11:05:02 -0500

    Relacion entre clases

--- models/models.py
+++ models/models.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class profesor(models.Model):
     _rec_name="apellido"
     _name="ga.profesor"
     _description="Profesor"
+    _order = "nombre ASC"
     nombre = fields.Char("Nombres",required=True)
     apellido = fields.Char("Apellidos",required=True)
     anioNacimiento = fields.Date("Año de Nacimiento")
@@ -14,7 +15,26 @@ class profesor(models.Model):
     edad = fields.Integer("Edad (años)")
     sueldo = fields.Float("Sueldo")
 
+    cursoIds= fields.One2many("ga.curso","profesorId")
 
+    @api.one
+    @api.constrains("sueldo")
+    def _check_sueldo(self):
+        if self.sueldo>10000 or self.sueldo<0:
+            raise ValidationError("El sueldo debe estar comprendido entre 0 y 10000")
+
+
+class curso(models.Model):
+    _name="ga.curso"
+    _description="Curso"
+
+    profesorId=fields.Many2one("ga.profesor")
+
+    name=fields.Char(string="Nombre del curso")
+    descripcion=fields.Html(string="Descripción del curso")
+    tiempo=fields.Integer(string="Tiempo(Horas)")
+    costo=fields.Monetary(string="Costo",currency_field="currency_id")
+    currency_id=fields.Many2one("res.currency",string="Moneda1")
 
 
 # class gestor_academico2(models.Model):
