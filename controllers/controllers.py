e0d991f0851f20d088dec285c49a070f60c1f2ac
Victor Daniel Moreno Lopez <danielvdmlfiis@gmail.com>
2017-11-26 13:08:21 -0500

    templates

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
+
 from odoo import http
+from odoo.http import request
+
+class home(http.Controller):
+
+    @http.route("/alumnos",auth="user")
+    def index(self):
+        Alumnos=request.env["ga.alumno"].search([])
+        """
+        str="<ul>"
+        for alumno in alumnos:
+            str=str+"<li>"+alumno.nombre+" "+alumno.apellido+"</li>"
+        str=str+"</ul>"
+        """
+
+        return request.render("gestor_academico2.listaAlumnos",{"alumnos":Alumnos})
+
+
+
+
+
 
 # class GestorAcademico2(http.Controller):
 #     @http.route('/gestor_academico2/gestor_academico2/', auth='public')
