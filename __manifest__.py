d16d6e8c7d732c8241740f7f806a83e7d1ee6da7
Victor Daniel Moreno Lopez <danielvdmlfiis@gmail.com>
2017-11-26 11:04:37 -0500

    grupos y permisos

--- __manifest__.py
+++ __manifest__.py
@@ -24,10 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'demo/demo.xml',
+        'security/ga_security.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
